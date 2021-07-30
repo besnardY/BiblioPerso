31cbd33b81ad7df108b87b66f54883e6dffeec72
BesnardY <yohann.besnard@outlook.fr>
2021-07-30 21:11:49 +0200

    add movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -3,20 +3,40 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Movie } from '../models/movie.model';
 import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+
   userDoc: AngularFirestoreDocument;
   imdbUrl = 'https://imdb-api.com/en/API';
+  omdbUrl ='http://www.omdbapi.com/?apikey=';
   imdbOptions = 'FullActor,FullCast,';
-  apiKey ='k_384w5ngh';
+  imdbApiKey ='';
+  omdbApiKey = '';
   idSearch: string;
   constructor(private http: HttpClient, private userServ: UserService) {
     this.userDoc = this.userServ.userDoc;
    }
+
+   getAllMovie() {
+    return this.userDoc
+      .collection('/movie', (ref) => ref.orderBy('title'))
+      .snapshotChanges()
+      .pipe(
+        map((actions) =>
+          actions.map((a) => {
+            const DATAS = a.payload.doc.data() as Movie;
+            const ID = a.payload.doc.id;
+            return { ID, ...DATAS };
+          })
+        )
+      );
+  }
 
   searchId(title: string): Observable<any>{
     console.log(title);
@@ -24,15 +44,27 @@ export class MovieService {
     if (!title.trim()) {
       return of([]);
     } else {
-      const REQ = `${this.imdbUrl}/Search/${this.apiKey}/${title}`;
+      const REQ = `${this.imdbUrl}/Search/${this.imdbApiKey}/${title}`;
+
 
       return this.http.get<any>(REQ);
     }
   }
 
   getMoviebyApi(id: string): Observable<any>{
-    const REQ = `${this.imdbUrl}/Title/${this.apiKey}/${id}/${this.imdbOptions}`;
+    //TODO if IMDB API
+    // const REQ = `${this.imdbUrl}/Title/${this.apiKey}/${id}/${this.imdbOptions}`;
 
+     //TODO if OMDB API
+    const REQ = `${this.omdbUrl}${this.omdbApiKey}${id}`;
       return this.http.get<any>(REQ);
+  }
+
+  addMovie(movie: Movie){
+    return this.userDoc.collection('/movie').add(movie);
+  }
+
+  updateMovie(updateMovie: Movie){
+
   }
 }
